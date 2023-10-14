f4e7ce0bc0c467c8a37da1c4befc5f8aadfe235a
LaraScazzola <lscazzola@fi.uba.ar>
2023-10-13 21:37:08 -0300

    Ya estan los endpoint para el feed, queda probarlos mas
    Co-authored-by: abrildiazmiguez <adiazm@fi.uba.ar>

--- repository/queries/queries_global.py
+++ repository/queries/queries_global.py
@@ -1,7 +1,7 @@
 """
 Archivo con algunas pruebas de la base de datos
 """
-from sqlalchemy import and_, exists, or_, func, select
+from sqlalchemy import and_, exists, literal_column, or_, func, select
 from sqlalchemy import desc
 from sqlalchemy.orm import aliased
 from sqlalchemy.orm import joinedload
@@ -47,13 +47,54 @@ def get_posts_from_users_followed_by_user_a(user_id_a):
 
     return posts
 
+#no va, por si algo falla
+def get_public_posts_user_is_interested_in3(user_id, amount, oldest_date) :
+    like_subquery = (
+        session.query(Like.id_post, func.count(Like.id_post).label("like_count"))
+        .group_by(Like.id_post)
+        .subquery()
+    )
+    repost_subquery = (
+        session.query(Repost.id_post, func.count(Repost.id_post).label("repost_count"))
+        .group_by(Repost.id_post)
+        .subquery()
+    )
+    i = aliased(Interests)
+    h = aliased(Hashtag)
+    p2 = aliased(Post)
+    u = aliased(User)
+
+    posts = (
+        session.query(
+            Post,
+            User,
+            like_subquery.c.like_count,
+            repost_subquery.c.repost_count
+        )
+        .join(i, i.user_id == user_id)
+        .join(h, i.interest == h.hashtag)
+        .join(p2, p2.id == h.id_post)
+        .filter(p2.posted_at < oldest_date)
+        .join(u, u.id == p2.user_id)
+        .group_by(Post.id, User.id)
+        .filter(u.is_public == True)
+        .outerjoin(like_subquery, Post.id == like_subquery.c.id_post)
+        .outerjoin(repost_subquery, Post.id == repost_subquery.c.id_post)
+        .order_by(desc(Post.posted_at))
+        .limit(amount)
+    )
+
+    return posts
+
+    
+
 def get_post_for_user_feed(user_id_a, n, date) :
     post_that_I_follow = get_posts_from_users_followed_by_user_a(user_id_a, int(n * 0.7), date)
     post_of_my_interest = get_public_posts_user_is_interested_in(user_id_a, int(n * 0.3), date)
     post_that_I_follow.extend(post_of_my_interest)
 
 #no va, por si algo falla
-def get_public_posts_user_is_interested_in2(user_id, amount, oldest_date) :
+def get_public_posts_user_is_interested_in(user_id, amount, oldest_date) :
     like_subquery = (
         session.query(Like.id_post, func.count(Like.id_post).label("like_count"))
         .group_by(Like.id_post)
@@ -70,57 +111,88 @@ def get_public_posts_user_is_interested_in2(user_id, amount, oldest_date) :
     u = aliased(User)
 
     query = (
-        session.query(
+        select(
             Post,
             User,
             like_subquery.c.like_count,
-            repost_subquery.c.repost_count,
+            repost_subquery.c.repost_count
         )
-        .join(User, User.id == Post.user_id)
-        .join(i, i.user_id == user_id) # Get all the user's interests
-        .join(h, h.hashtag == i.interest) # Get the corresponding hashtags and posts where the hashtag appears
-        .group_by(Post.id)  # Ensure no post is shown twice
-        .join(p2, p2.id == h.id_post) # Get all the posts info that the user is interested in
-        .join(u, u.id == p2.user_id) # Get all the user info from the user that published the post
-        .filter(u.is_public == True)  # Only keep the posts made by public users
-        .outerjoin(like_subquery, Post.id == like_subquery.c.id_post)
-        .outerjoin(repost_subquery, Post.id == repost_subquery.c.id_post)
-        .filter(Post.posted_at < oldest_date)
-        .order_by(desc(Post.posted_at))  # Order by date
-        .limit(amount)  # How many of the top results I want
+        .select_from(
+            Post.__table__
+            .join(User, User.id == Post.user_id)
+            .join(i, i.user_id == user_id)
+            .join(h, i.interest == h.hashtag)
+            .join(p2, p2.id == h.id_post)
+            #.join(u, p2.user_id == u.id)
+            .outerjoin(like_subquery, p2.id == like_subquery.c.id_post)
+            .outerjoin(repost_subquery, p2.id == repost_subquery.c.id_post)
+        )
+        .where(u.is_public == True)
+        .where(p2.posted_at < oldest_date)
+        .order_by(desc(p2.posted_at))
+        .limit(amount)
     )
 
+    print("LO QUE DEV LA QUERIE")
+    print(session.execute(query))
     return session.execute(query)
 
 
-def get_public_posts_user_is_interested_in(user_id, amount, oldest_date) :
-    # Create aliases for the necessary tables
+
+def get_public_posts_user_is_interested_in2(user_id, amount, oldest_date) :
+    # Crea subconsultas para los recuentos de likes y reposts
+    like_subquery = (
+        session.query(
+            Like.id_post,
+            func.count(Like.id_post).label("like_count")
+        )
+        .group_by(Like.id_post)
+        .subquery()
+    )
+
+    repost_subquery = (
+        session.query(
+            Repost.id_post,
+            func.count(Repost.id_post).label("repost_count")
+        )
+        .group_by(Repost.id_post)
+        .subquery()
+    )
+
+    # Crea aliases para las tablas
     i = aliased(Interests)
     h = aliased(Hashtag)
     p2 = aliased(Post)
     u = aliased(User)
 
+    # Crea la consulta principal
     query = (
-        select(Post, User)
+        select([
+            Post,
+            User,
+            like_subquery.c.like_count,
+            repost_subquery.c.repost_count
+        ])
         .select_from(
             Post.__table__
-            .join(i, i.user_id == user_id) # Get all the user's interests
+            .join(i, i.user_id == user_id)
             .join(h, i.interest == h.hashtag)
             .join(p2, p2.id == h.id_post)
             .join(u, u.id == p2.user_id)
         )
-        #.join(i, i.user_id == user_id) # Get all the user's interests
-        #.join(h, h.hashtag == i.interest) # Get the corresponding hashtags and posts where the hashtag appears
-        .group_by(Post.id, User.id)  # Ensure no post is shown twice
-        #.join(p2, p2.id == h.id_post) # Get all the posts info that the user is interested in
-        #.join(u, u.id == p2.user_id) # Get all the user info from the user that published the post
-        .filter(u.is_public == True)  # Only keep the posts made by public users
-        .filter(p2.posted_at < oldest_date) #Only keep those posts that are older to this date
-        .order_by(desc(Post.posted_at))  # Order by date
-        .limit(amount)  # How many of the top results I want
+        .group_by(Post.id, User.id)
+        .filter(u.is_public == True)
+        .filter(p2.posted_at < oldest_date)
+        .order_by(desc(Post.posted_at))
+        .limit(amount)
     )
 
-    return session.execute(query)
+    # Realiza el outerjoin con las subconsultas de likes y reposts
+    query = query.outerjoin(like_subquery, like_subquery.c.id_post == Post.id)
+    query = query.outerjoin(repost_subquery, repost_subquery.c.id_post == Post.id)
+
+    # Ejecuta la consulta y obtén los resultados
+    result = session.execute(query)
 
 def get_posts_from_users_followed_by_user_a(user_id_a, n, date):
     like_subquery = (
