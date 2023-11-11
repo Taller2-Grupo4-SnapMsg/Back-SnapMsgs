499d4f6ec0809b4766c3ad23b7910f499446deb9
Merok23 <alejocalizblanco@gmail.com>
2023-11-11 20:14:38 -0300

    FIX: Se cambio el nombre del token en los endpoints de token-admin a token (necesitamos ese gateway). Se borraron 2 carpetas de pycache que no fueron git ignoradas hace 2 meses atras(?

--- control/controller_post.py
+++ control/controller_post.py
@@ -170,7 +170,7 @@ async def api_get_posts_for_admin_user_id(
     email: str,
     start: int = Query(0, title="start", description="start for pagination"),
     ammount: int = Query(10, title="ammount", description="ammount of posts"),
-    admin_token: str = Header(...),
+    token: str = Header(...),
 ):
     """
     This endpoint is used for admins, and is used to retrieve posts from
@@ -179,14 +179,14 @@ async def api_get_posts_for_admin_user_id(
     param: user_email: The email of the user to retrieve posts from
     param: start: The starting point of the pagination
     param: offset: The offset of the pagination
-    param: admin_token: The token of the admin.
+    param: token: The token of the admin.
 
     return: A list of posts
     """
     try:
         if ammount > MAX_AMMOUNT:
             raise HTTPException(status_code=400, detail="Max ammount is 25")
-        if not await token_is_admin(admin_token):
+        if not await token_is_admin(token):
             raise HTTPException(status_code=403, detail="Invalid token")
         user_id = get_user_id_from_email(email)
         posts_db = get_posts_and_reposts_for_admin_user_id(user_id, start, ammount)
@@ -201,20 +201,21 @@ async def api_get_posts_for_admin_user_id(
 async def api_get_posts_for_admin(
     start: int = Query(0, title="start", description="start for pagination"),
     ammount: int = Query(10, title="ammount", description="ammount of posts"),
-    admin_token: str = Header(...),
+    token: str = Header(...),
 ):
     """
     This endpoint is used for admins, and is used to retrieve posts in a paginated way.
 
     param: start: The starting point of the pagination
     param: offset: The offset of the pagination
+    param: token: The token of the ADMIN.
 
     return: A list of posts with no filters
     """
     try:
         if ammount > MAX_AMMOUNT:
             raise HTTPException(status_code=400, detail="Max ammount is 25")
-        if not await token_is_admin(admin_token):
+        if not await token_is_admin(token):
             raise HTTPException(status_code=403, detail="Invalid token")
         posts_db = get_posts_and_reposts_for_admin(start, ammount)
         return generate_response_posts_from_db_for_admin(posts_db)
